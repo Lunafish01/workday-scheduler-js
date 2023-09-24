5ed43c2a5caea2efc0db1192dbc64a9add2ecf87
Lunafish01 <142092354+Lunafish01@users.noreply.github.com>
2023-09-24 17:56:24 -0500

    updated html and script files

--- script.js
+++ script.js
@@ -12,12 +12,18 @@ $(document).ready(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+ 
+ 
+ 
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+
+
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
@@ -30,5 +36,10 @@ $(document).ready(function () {
     //   localStorage.setItem(time, value);
     // });
 
+    // $("#hour-9 .time-block").val(localStorage.getItem('text-center'));
+
+
   // TODO: Add code to display the current date in the header of the page.
+  var dateAndTime = dayjs().format('dddd MMM DD, YYYY hh:mm:ss A');
+  $('#currentDay').text(dateAndTime);
 });
