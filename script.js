abf8c893c0becda5d890c404e19e64f6075ee3a2
Lunafish01 <142092354+Lunafish01@users.noreply.github.com>
2023-09-25 23:11:10 -0500

    updated html and script files

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-//jquery wrapping to have html load before javascript
+//jquery wrapping to load DOM first
 $(document).ready(function () {
  
- 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
 // declare variable to store current time
 var currentHour = dayjs().hour().toString();
 
 // get a handle on all elements with class of 'time-block'
-// get 'id' attribute and split off time value from 'id'
+// get 'id' attribute and get just the hour number from the 'id'
 $(".time-block").each(function() {
   var timeBlock = +$(this).attr("id").split("-")[1];
 
@@ -32,29 +21,29 @@ $(".time-block").each(function() {
   }
 });
 
-  // selecting and targeting all button elements with the class selector of '.saveBtn'
-  // adding event handler function to all button elements listening for a 'click' event
-  // add preventDefault to keep browser from refreshing 
+// selecting and targeting all button elements with the class selector of '.saveBtn'
+// adding event handler function to all button elements listening for a 'click' event
+// add preventDefault to keep browser from refreshing 
     $(".saveBtn").click(function (event){
       event.preventDefault();
 
-  // declaring variables for 'value' for 'time'
-  // store time and data input entered in textarea elements
+// declaring variables for 'value' for 'time'
+// store time and data input entered in textarea elements
       var value = $(this).siblings(".description").val();
-  // use 'split' method to parse 'id' element and return second index object
+// use 'split' method to parse 'id' element and return second index object
       var time = $(this).parent().attr("id").split("-")[1];
 
-  // store the data input and time in local storage
+// store the data input and time in local storage
       localStorage.setItem(time, value);
     });
 
-  // intiate for loop set saved data in local storage 
-    for (var i = 0; i <= 10; i++) {
+// intiate for loop set saved data in local storage 
+    for (var i = 9; i <= 18; i++) {
       var savedValue = localStorage.getItem(i.toString());
       $("#hour-" + i + " .description").val(savedValue);
     }
 
-  // added date and time to the top of the page using Dayjs library.
+// added date and time to the top of the page using Dayjs library.
   var date = dayjs().format('dddd MMM DD, YYYY hh:mm:ss A');
   $('#currentDay').text(date);
 });
